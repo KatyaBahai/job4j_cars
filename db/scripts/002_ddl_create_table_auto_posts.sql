7701423721b895d650c9e62170070e7b988152aa
KatyaBahai <katyxa-two@yandex.ru>
2025-02-05 22:02:32 +0300

    changed data type of creation_date to timestamp

--- db/scripts/002_ddl_create_table_auto_posts.sql
+++ db/scripts/002_ddl_create_table_auto_posts.sql
@@ -2,6 +2,6 @@ create table auto_posts
 (
     id   serial primary key,
     description varchar not null,
-    creation_date varchar not null unique,
+    creation_date timestamp not null unique,
     auto_user_id int references auto_users (id) not null
 );
